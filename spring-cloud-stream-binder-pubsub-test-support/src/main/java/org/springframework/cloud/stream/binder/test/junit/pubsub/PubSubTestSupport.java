651eb26493f410752761d39dade2d9cba26422ad
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2016-09-29 22:15:47 -0400

    Adding BinderTest support, local pubsub emulator

---
+++ spring-cloud-stream-binder-pubsub-test-support/src/main/java/org/springframework/cloud/stream/binder/test/junit/pubsub/PubSubTestSupport.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright 2016 original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.springframework.cloud.stream.binder.test.junit.pubsub;
+
+import java.io.ByteArrayInputStream;
+
+import com.google.cloud.AuthCredentials;
+import com.google.cloud.pubsub.PubSub;
+import com.google.cloud.pubsub.PubSubOptions;
+import com.google.cloud.pubsub.testing.LocalPubsubHelper;
+
+import org.springframework.cloud.stream.test.junit.AbstractExternalResourceTestSupport;
+import org.springframework.util.StringUtils;
+
+/**
+ * @author Vinicius Carvalho
+ */
+public class PubSubTestSupport extends AbstractExternalResourceTestSupport<PubSub> {
+
+	public PubSubTestSupport() {
+		super("PUBSUB");
+	}
+
+	@Override
+	protected void cleanupResource() throws Exception {
+
+	}
+
+	@Override
+	protected void obtainResource() throws Exception {
+		if(StringUtils.hasText(System.getenv("GOOGLE_CLOUD_JSON_ENV"))){
+			resource = PubSubOptions
+					.builder()
+					.authCredentials(AuthCredentials.createForJson(new ByteArrayInputStream(System.getenv("GOOGLE_CLOUD_JSON_ENV").getBytes()))).build().service();
+		}else{
+			LocalPubsubHelper helper = LocalPubsubHelper.create();
+			helper.start();
+			resource = helper.options().service();
+
+		}
+	}
+}
